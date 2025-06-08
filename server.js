ac6e55685dec72e7b1242de6aa91748a989b643d
deniz <denizcan.boyraz@gmail.com>
2025-06-08 12:00:49 +0300

    remove railway from cors

--- server.js
+++ server.js
@@ -82,8 +82,7 @@ setInterval(async () => {
 
 app.use(cors({
   origin: [
-    process.env.FRONTEND_URL || "https://web-app-psi-nine.vercel.app/",
-    "https://server-production-84d1.up.railway.app/"
+    process.env.FRONTEND_URL || "https://web-app-psi-nine.vercel.app/"
   ],
   credentials: true
 })); 
